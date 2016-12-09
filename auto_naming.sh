db6dbcadc46fc1296557124917f9aeff60cfb6d3
Takuto Sugita <sugita_t@worksap.co.jp>
2016-12-09 19:10:22 +0900

    add

--- auto_naming.sh
+++ auto_naming.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
  DIR=$1
- echo $DIR 
  FILE_NUM_BERORE=0
  FILE_NAME=""
 # get the number of files in the specified directory
@@ -17,19 +16,34 @@ get_current_file_name() {
 
 # modify the file name based on the last modified name
 add_one_to_last_modified() {
+	latest_modified_file_name=$FILE_NAME
+	# split the string by "-"
+	IFS="-"
+	set -- $latest_modified_file_name
+	suffix=`expr $2 + 1`
+	suffix=`printf %04d $suffix`
+	new_file_name="${1}-${suffix}"
+}
 
-#get_file_number
-#if filenum >= FILE_NUM_BEFORE
-# get_current_file_name
-# if filenum == FILE_NUM_BEFORE
-#  change the current file name based on the user input
-# else 
-#  if filenum == 1
-#   FILE_NAME="0001-001"
-#   mv file_name FILE_NAME
-#  else
-#   add 1 to the file last modified
-#fi
+
+# only when the folder is updated somehow, the process below starts
+if 
+get_file_number
+if [ filenum -ge FILE_NUM_BEFORE ] ; then
+ 	get_current_file_name
+ 	if [ filenum = FILE_NUM_BEFORE ] ; then
+#  		change the current file name based on the user input
+ 	else 
+  		if [ filenum = 1 ] ; then
+  			FILE_NAME="0001-001"
+   			mv file_name FILE_NAME
+  		else
+			add_one_to_last_modified
+			mv file_name new_file_name
+			FILE_NAME=new_file_name
+		fi
+	fi
+fi
 
 
 
--- string_addition.sh
+++ string_addition.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-file="0001-002"
-echo file+1
-file=`expr $file + 1`
-echo $file
+file="0001-0043"
+echo "File name is ${file}" 
+
+# split the string by "-"
+IFS="-"
+set -- $file
+suffix=`expr $2 + 1`
+suffix=`printf %04d $suffix`
+echo "Modified file name is: ${1}-${suffix}"
