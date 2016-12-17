5ebed6fa70bc8e613102336d2bda3098e97b358c
Takuto <s.takuto0214@gmail.com>
2016-12-17 12:40:19 +0900

    add the textbox and circle object attachment function

--- python/excel_automation/xlsxwriter_demo.py
+++ python/excel_automation/xlsxwriter_demo.py
@@ -10,24 +10,30 @@ files = sorted(files)
 height = 20
 current_pic_prefix = '0001'
 xth_picture = 0
-options = {
-        'font': {'size': 14},
+options_for_textbox = {
+        'font': {'size': 14,
+            'color': 'red'},
         'align': {'horizontal':'center'},
         'line': { 'color': 'red',
             'width': 2.25},
-        'fill': {'color': '#81d4fa'}
-        }
+        'fill': {'color': '#81d4fa'},
+}
+options_for_graphical_circle = {
+        'line': {'color': 'red',
+            'width': 2.25},
+        'fill': {'none': True},
+}
+text = ""
 for file in files:
     if file.find('.jpeg') > -1:
-        print("this is about to be processed " + file)
         first, second = file.split('-')
-        print(first)
         if current_pic_prefix == first:
             xth_picture = xth_picture + 1
             y_position = 1 + height * (xth_picture - 1)
             worksheet.write(y_position - 1, 1, file)
             worksheet.insert_image(y_position, 1, file)
-            worksheet.insert_textbox(y_position, 0, options)
+            worksheet.insert_textbox(y_position, 0, text, options_for_textbox)
+            worksheet.insert_textbox(y_position + 1, 0, text, options_for_graphical_circle)
         else:
             current_pic_prefix = first
             xth_picture = 1
@@ -38,6 +44,7 @@ for file in files:
             worksheet = workbook.add_worksheet(current_pic_prefix)
             worksheet.write(y_position - 1, 1, file)
             worksheet.insert_image(y_position, 1, file)
-            worksheet.insert_textbox(y_position, 0, options)
+            worksheet.insert_textbox(y_position, 0, text, options_for_textbox)
+            worksheet.insert_textbox(y_position + 1, 0, text, options_for_graphical_circle)
 workbook.close()
 
